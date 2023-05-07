5ecdcdf24e6825fe0eb0a766377e6633eee73c84
Ossy Lab <ossyisibor@gmail.com>
2023-05-07 23:40:45 +0100

    updated the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,83 +1,29 @@
-const isDevelopment = process.env.NODE_ENV === "development";
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
+  mode: "production",
   entry: path.resolve(__dirname, "./src/index.js"),
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
-
-      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
-    }),
-  ],
+  devtool: "source-map",
   module: {
     rules: [
-      // {
-      //   test: /\.(scss|css)$/,
-      //   use: [
-      //     MiniCssExtractPlugin.loader,
-      //     "style-loader",
-      //     "css-loader",
-      //     "sass-loader",
-      //   ],
-      // },
-
       {
-        test: /\.module\.s(a|c)ss$/,
-
-        loader: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-
-          {
-            loader: "css-loader",
-
-            options: {
-              modules: true,
-
-              sourceMap: isDevelopment,
-            },
-          },
-
-          {
-            loader: "sass-loader",
-
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
       },
-
       {
-        test: /\.s(a|c)ss$/,
-
-        exclude: /\.module.(s(a|c)ss)$/,
-
-        loader: [
-          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
-
-          "css-loader",
-
-          {
-            loader: "sass-loader",
-
-            options: {
-              sourceMap: isDevelopment,
-            },
-          },
-        ],
+        test: /\.(s(a|c)ss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ["*", ".js", ".jsx", ".scss"],
+    extensions: ["*", ".js", ".jsx"],
   },
+  plugins: [new MiniCssExtractPlugin()],
   output: {
-    path: path.resolve(__dirname, "./dist"),
+    path: path.resolve(__dirname, "./public"),
     filename: "bundle.js",
-  },
-  devServer: {
-    static: path.resolve(__dirname, "./dist"),
   },
 };
