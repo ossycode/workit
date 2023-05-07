84bdf1e882692a47820cfb6cd5249149bcb1af86
Ossy Lab <ossyisibor@gmail.com>
2023-05-08 00:10:53 +0100

    setup deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,22 @@
 const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-  mode: "production",
   entry: path.resolve(__dirname, "./src/index.js"),
-  devtool: "source-map",
+
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(s(a|c)ss)$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        test: /\.(scss|css)$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-  resolve: {
-    extensions: ["*", ".js", ".jsx"],
-  },
-  plugins: [new MiniCssExtractPlugin()],
+
   output: {
-    path: path.resolve(__dirname, "./public"),
+    path: path.resolve(__dirname, "./dist"),
     filename: "bundle.js",
+  },
+  devServer: {
+    static: path.resolve(__dirname, "./dist"),
   },
 };
