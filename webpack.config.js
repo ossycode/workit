0a6fa891d9e854450410f62157aa54a42f038396
Ossy Lab <ossyisibor@gmail.com>
2023-05-07 23:32:24 +0100

    updated the webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -3,22 +3,74 @@ const path = require("path");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/index.js"),
-  plugins: [new MiniCssExtractPlugin()],
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: isDevelopment ? "[name].css" : "[name].[hash].css",
+
+      chunkFilename: isDevelopment ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
   module: {
     rules: [
+      // {
+      //   test: /\.(scss|css)$/,
+      //   use: [
+      //     MiniCssExtractPlugin.loader,
+      //     "style-loader",
+      //     "css-loader",
+      //     "sass-loader",
+      //   ],
+      // },
+
       {
-        test: /\.(scss|css)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          "style-loader",
+        test: /\.module\.s(a|c)ss$/,
+
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+
+          {
+            loader: "css-loader",
+
+            options: {
+              modules: true,
+
+              sourceMap: isDevelopment,
+            },
+          },
+
+          {
+            loader: "sass-loader",
+
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.s(a|c)ss$/,
+
+        exclude: /\.module.(s(a|c)ss)$/,
+
+        loader: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+
           "css-loader",
-          "sass-loader",
+
+          {
+            loader: "sass-loader",
+
+            options: {
+              sourceMap: isDevelopment,
+            },
+          },
         ],
       },
     ],
   },
   resolve: {
-    extensions: ["*", ".js"],
+    extensions: ["*", ".js", ".jsx", ".scss"],
   },
   output: {
     path: path.resolve(__dirname, "./dist"),
